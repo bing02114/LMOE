2d51e1e61c228cdf90f6f97b0a890456a32f1fb4
bing02114 <1017257504@qq.com>
2023-12-31 21:44:25 +0800

    feat: first commit

---
+++ LMOE-platform/src/test/java/org/tju/lmoeplatform/LmoePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tju.lmoeplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmoePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
